4c39cac9f50f05f1db41a0b5a80b1bd24c0e4e4d
Elan Ruusamäe <glen@pld-linux.org>
2023-02-07 09:00:56 +0200

    Add diagnostic messages to show what is going on

--- src/yarn-install-dev.ts
+++ src/yarn-install-dev.ts
@@ -17,14 +17,22 @@ const addDeps = async (packagePath: string, dependencies: string[]): Promise<voi
 
   // Copy from devDependencies
   for (const dep of dependencies) {
+    console.info(`Copying from devDependencies: ${dep}`);
     pkg.dependencies[dep] = pkg.devDependencies[dep];
   }
 
   await writeFile(packagePath, JSON.stringify(pkg));
+  console.debug(`Wrote ${packagePath}`, pkg);
   await exec("yarn install --frozen-lockfile --production");
 };
 
 const main = async (dependencies: string[]): Promise<void> => {
+  if (!dependencies.length) {
+    console.warn("No dependencies to install; Skipping.");
+
+    return;
+  }
+
   const packagePath = await findUp("package.json");
 
   if (!packagePath) {
@@ -33,12 +41,14 @@ const main = async (dependencies: string[]): Promise<void> => {
 
   const backupPath = `${packagePath}.bak`;
 
+  console.info(`Creating backup: ${packagePath} -> ${backupPath}`);
   await copyFile(packagePath, backupPath);
 
   try {
     await addDeps(packagePath, dependencies);
   } finally {
     await copyFile(backupPath, packagePath);
+    console.info(`Restored backup: ${backupPath} -> ${packagePath}`);
   }
 };
 
